c41a74615dd4266624398b4f99ce2b71525a8a62
eugen1701 <eugen.flocea@yahoo.com>
2022-03-08 15:52:38 +0200

    First commit

---
+++ src/test/java/FirstTests.java
@@ -0,0 +1,65 @@
+import domain.Student;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import repository.NotaXMLRepository;
+import repository.TemaXMLRepository;
+import service.Service;
+import repository.StudentXMLRepository
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Collections;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class FirstTests {
+
+    private Service service;
+
+    @BeforeEach
+    public void setUp() {
+        String filenameStudent = "src/test/resources/fisiere/Studenti.xml";
+        String filenameTema = "src/test/resources/fisiere/Teme.xml";
+        String filenameNota = "src/test/resources/fisiere/Note.xml";
+
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        NotaValidator notaValidator = new NotaValidator();
+
+        StudentXMLRepository studentXMLRepository = new StudentXMLRepository(studentValidator, filenameStudent);
+        TemaXMLRepository temaXMLRepository = new TemaXMLRepository(temaValidator, filenameTema);
+        NotaXMLRepository notaXMLRepository = new NotaXMLRepository(notaValidator, filenameNota);
+
+
+        service = new Service(studentXMLRepository, temaXMLRepository, notaXMLRepository);
+    }
+
+    @AfterEach
+    public void tearDown() throws IOException {
+        Path file = Paths.get("src/test/resources/fisiere/Studenti.xml");
+        Files.write(file, Collections.singletonList("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><inbox></inbox>"), StandardCharsets.UTF_8);
+        file = Paths.get("src/test/resources/fisiere/Teme.xml");
+        Files.write(file, Collections.singletonList("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><inbox></inbox>"), StandardCharsets.UTF_8);
+        file = Paths.get("src/test/resources/fisiere/Note.xml");
+        Files.write(file, Collections.singletonList("<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"no\"?><inbox></inbox>"), StandardCharsets.UTF_8);
+
+    }
+
+    @Test
+    public void addStudent_validStudentGroup() {
+        Student student = new Student("123", "name", 100);
+        int group  = 933;
+        String id = "123";
+        String name = "Fineas";
+        assertEquals(service.saveStudent(id, name, group), 1);
+    }
+
+    
+}
