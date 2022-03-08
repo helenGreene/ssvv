e3ba92d2436f340f8d0301b1d7267bc3dfc15139
eugen1701 <eugen.flocea@yahoo.com>
2022-03-08 16:10:34 +0200

    Add import

--- src/test/java/FirstTests.java
+++ src/test/java/FirstTests.java
@@ -11,6 +11,7 @@ import validation.StudentValidator;
 import validation.TemaValidator;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
