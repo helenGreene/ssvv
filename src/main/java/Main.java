4667fc8e2fc103a1b326623a67d3075eba0b8b53
EFlocea <eugen.flocea@microfocus.com>
2023-03-22 18:18:22 +0200

    try to add a vulnerability

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,6 +9,9 @@ import validation.*;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.ObjectInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Main {
     public static void main(String[] args) {
@@ -39,6 +42,22 @@ public class Main {
             Student userData = (Student) ois.readObject();
             ois.close();
         } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void pleaseAddVulnerability() {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
+                    "user=steve&password=blue"); // Sensitive
+            String uname = "steve";
+            String password = "blue";
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/test?" +
+                    "user=" + uname + "&password=" + password); // Sensitive
+
+            java.net.PasswordAuthentication pa = new java.net.PasswordAuthentication("userName", "1234".toCharArray());  // Sensitive
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
